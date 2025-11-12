e0e8e388df29562d75df10f1c1ff93d9ca85c9af
Tansholpan <ttelbayeva@innoforce.kz>
2025-11-12 13:35:50 +0300

    BUILD UP PROJECT

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,5 +1,4 @@
-#!/usr/bin/env bash
-set -euo pipefail
-docker compose down -v || true
-rm -rf data/kafka-1 data/kafka-2 data/kafka-3 data/minio || true
-mkdir -p data/kafka-1 data/kafka-2 data/kafka-3
+docker compose down -v
+rm -rf ./kafka/data/*
+docker compose up -d
+docker logs -f kafka
