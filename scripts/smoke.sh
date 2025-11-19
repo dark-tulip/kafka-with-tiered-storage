53314d4666a476119db17d3646242c065979a4c5
Tansholpan <ttelbayeva@innoforce.kz>
2025-11-20 00:17:39 +0300

    added new board for RSM metrics

--- scripts/smoke.sh
+++ scripts/smoke.sh
@@ -1,9 +1,9 @@
 docker exec \
   -e JMX_PORT= -e KAFKA_JMX_PORT= -e RMI_HOSTNAME= -e KAFKA_JMX_HOSTNAME= -e KAFKA_OPTS= -it kafka /opt/kafka/bin/kafka-producer-perf-test.sh \
                                          --topic test-tiered \
-                                         --num-records 100000 \
+                                         --num-records 1000000 \
                                          --throughput -1 \
-                                         --record-size 1000 \
+                                         --record-size 10000 \
                                          --producer-props acks=1 batch.size=16384 bootstrap.servers=kafka:9092
 
 
